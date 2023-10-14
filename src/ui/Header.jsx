da870932637f866018d522de82d9ad6b5b4e1493
azerrors <azer.nagi.2005@gmail.com>
2023-10-15 00:36:25 +0400

    deleting icon

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -6,11 +6,7 @@ function Header() {
   return (
     <div className="flex items-center  justify-between p-4">
       <Link to="/">
-        <img
-          src="/public/finallogo.jpg"
-          className="h-20 w-20 mix-blend-color-dodge"
-          alt=""
-        />
+      
       </Link>
         <NavLink />
     </div>
